67bb4236d2ef6739691a9c10dccf4f18072b4a3a
Lucas <80419418+lsena-web@users.noreply.github.com>
2021-12-25 14:05:51 -0300

    paginaçãp

--- app/Controller/Pages/Testimony.php
+++ app/Controller/Pages/Testimony.php
@@ -8,13 +8,45 @@ use \App\Model\Entity\Testimony as EntityTestimony;
 class Testimony extends Page
 {
     /**
+     * Método responsável por obter a renderização dos itens de depoimentos para a página
+     * @return string 
+     */
+    private static function getTestimonyItems()
+    {
+        // DEPOIMENTOS
+        $itens = '';
+
+        // RESULTADOS DA PÁGINA
+        $results = EntityTestimony::getTestimonies(null, 'id DESC');
+
+        // RENDERIZA O ITEM
+        while ($obTestimony = $results->fetchObject(EntityTestimony::class)) {
+            echo '<pre>';
+            print_r ($obTestimony);
+            echo '</pre>';
+            exit;
+            
+            $itens .= View::render('pages/testimony/item', [
+                'nome' => $obTestimony->nome,
+                'mensagem' => $obTestimony->mensagem,
+                'data' => date('d/m/Y H:i:s', strtotime($obTestimony->data))
+            ]);
+        }
+
+        // RETORNA OS DEPOIMENTOS
+        return $itens;
+    }
+
+    /**
      * Método responsável por retornar o conteúdo view da nossa home
      * @return string
      */
     public static function getTestimonies()
     {
         // VIEW DE DEPOIMENTOS
-        $content = View::render('pages/testimonies', []);
+        $content = View::render('pages/testimonies', [
+            'itens' => self::getTestimonyItems()
+        ]);
         // RETORNA A VIEW DA PÁGINA
         return parent::getPage('DEPOIMENTOS > MVC', $content);
     }
@@ -34,7 +66,7 @@ class Testimony extends Page
         $obTestimony->nome = $postVars['nome'];
         $obTestimony->mensagem = $postVars['mensagem'];
         $obTestimony->cadastrar();
-        
+
         return self::getTestimonies();
     }
 }
--- app/Model/Entity/Testimony.php
+++ app/Model/Entity/Testimony.php
@@ -51,4 +51,17 @@ class Testimony
         // SUCESSO
         return true;
     }
+
+    /**
+     * Método responsável por retornar depoimentos
+     * @param string $where
+     * @param string $order
+     * @param string $limit
+     * @param string $field
+     * @return PDOStatement
+     */
+    public static function getTestimonies($where = null, $order = null, $limit = null, $fields = '*')
+    {
+        return (new Database('depoimentos'))->select($where, $order, $limit, $fields);
+    }
 }
